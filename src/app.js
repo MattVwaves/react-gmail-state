4b00e67df0c8259e19a9cfa7d5ad3b82defee1e2
Joel Watkins <eck087@yahoo.co.uk>
2022-11-08 17:57:58 +0000

    started extension 2

--- src/app.js
+++ src/app.js
@@ -1,42 +1,51 @@
-import Header from './components/header'
-import initialEmails from './data/emails'
-import {useState} from 'react'
+import Header from "./components/header";
+import initialEmails from "./data/emails";
+import { useState } from "react";
 
-import './styles/app.css'
+import "./styles/app.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
-  const [emails, setEmails] = useState(initialEmails)
+  const [emails, setEmails] = useState(initialEmails);
+  const [showEmails, setShowEmails] = useState(true);
+  const [hideRead, setHideRead] = useState(false);
 
-const toggleRead = (email)=>{
-  console.log(email)
-  const read = emails.map((storedEmail)=>{
-    if (storedEmail === email){
-      const emailCopy = {...email, read: !email.read}
-      return emailCopy
+  console.log(hideRead);
+
+  const toggleRead = (email) => {
+    console.log(email);
+    const read = emails.map((storedEmail) => {
+      if (storedEmail === email) {
+        const emailCopy = { ...email, read: !email.read };
+        return emailCopy;
+      } else {
+        return storedEmail;
+      }
+    });
+    setEmails(read);
+  };
+  const toggleStarred = (email) => {
+    console.log(email);
+    const read = emails.map((storedEmail) => {
+      if (storedEmail === email) {
+        const emailCopy = { ...email, starred: !email.starred };
+        return emailCopy;
+      } else {
+        return storedEmail;
+      }
+    });
+    setEmails(read);
+  };
+
+  const toggleReadList = () => {
+    setHideRead(!hideRead);
+    setShowEmails(!showEmails);
+  };
+  const unreadEmails = emails.map((email) => {
+    if (email.read === false) {
+      return email;
     }
-    else {
-      return storedEmail
-    }
-    
-  })
-  setEmails(read)
-}
-const toggleStarred = (email)=>{
-  console.log(email)
-  const read = emails.map((storedEmail)=>{
-    if (storedEmail === email){
-      const emailCopy = {...email, starred: !email.starred}
-      return emailCopy
-    }
-    else {
-      return storedEmail
-    }
-    
-  })
-  setEmails(read)
-}
+  });
 
   return (
     <div className="app">
@@ -63,35 +72,68 @@ const toggleStarred = (email)=>{
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              // checked={false}
+              onChange={() => toggleReadList()}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{
-        emails.map((email)=>{
-          return <li key={email.id} className={email.read ? 'email read': 'email unread'}>
-          <div  className="select">
-          <input
-            onChange={() => toggleRead(email)}
-            className="select-checkbox"
-            type="checkbox"/>
-          </div>
-          <div className="star">
-          <input
-            onChange={() => toggleStarred(email)}
-            className="star-checkbox"
-            type="checkbox"
-          />
-          </div>
-          <div className="sender">{email.sender}</div>
-          <div className="title">{email.title}</div>
-        </li>
-        })  
-      }</main>
+      <main className="emails">
+        {showEmails &&
+          emails.map((email) => {
+            return (
+              <li
+                key={email.id}
+                className={email.read ? "email read" : "email unread"}
+              >
+                <div className="select">
+                  <input
+                    onChange={() => toggleRead(email)}
+                    className="select-checkbox"
+                    type="checkbox"
+                  />
+                </div>
+                <div className="star">
+                  <input
+                    onChange={() => toggleStarred(email)}
+                    className="star-checkbox"
+                    type="checkbox"
+                  />
+                </div>
+                <div className="sender">{email.sender}</div>
+                <div className="title">{email.title}</div>
+              </li>
+            );
+          })}
+        {hideRead &&
+          unreadEmails.map((email) => {
+            return (
+              <li
+                key={email.id}
+                className={email.read ? "email read" : "email unread"}
+              >
+                <div className="select">
+                  <input
+                    onChange={() => toggleRead(email)}
+                    className="select-checkbox"
+                    type="checkbox"
+                  />
+                </div>
+                <div className="star">
+                  <input
+                    onChange={() => toggleStarred(email)}
+                    className="star-checkbox"
+                    type="checkbox"
+                  />
+                </div>
+                <div className="sender">{email.sender}</div>
+                <div className="title">{email.title}</div>
+              </li>
+            );
+          })}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
